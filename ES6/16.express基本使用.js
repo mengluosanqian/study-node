7a271c88d8cea41d46467ed9e3b87a90a0925366
liuwei <1033615602@qq.com>
2021-09-13 10:34:13 +0800

    格式化

--- ES6/16.express基本使用.js
+++ ES6/16.express基本使用.js
@@ -1,17 +1,17 @@
 // 1.引入express
-const express = require('express');
+const express = require('express')
 // 2.创建应用对象
-const app = express();
+const app = express()
 // 3.创建路由规则
 // request 是对请求报文的封装
 // response 是对响应报文的封装
-app.get('/',(request,response) => {
-    // 设置响应
-    response.send('HELLO EXPRESS')
+app.get('/', (request, response) => {
+// 设置响应
+  response.send('HELLO EXPRESS')
 })
 // 4.监听端口启动服务
-app.listen(8000, ()=>{
-    console.log('服务已启动');
+app.listen(8000, () => {
+  console.log('服务已启动')
 })
 // 5.浏览器查看 127.0.0.1:8000
 
