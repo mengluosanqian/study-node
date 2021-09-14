9b2f567bd95afa7d7328295a940780e3ec7d0638
liuwei <1033615602@qq.com>
2021-09-14 15:43:35 +0800

    修改promise

--- ES6/js/promise-server.js
+++ ES6/js/promise-server.js
@@ -1,14 +1,14 @@
 // 1.引入express
-const express = require('express');
+const express = require('express')
 // 2.创建应用对象
-const app = express();
+const app = express()
 app.all('/cors-server', (request, response) => {
-    //设置响应头
-    response.setHeader('Access-Control-Allow-Origin', '*')
-    response.setHeader('Access-Control-Allow-Headers', '*')
-    response.setHeader('Access-Control-Allow-Method', '*')
-    response.send('hello') //只接受字符串类型
+  // 设置响应头
+  response.setHeader('Access-Control-Allow-Origin', '*')
+  response.setHeader('Access-Control-Allow-Headers', '*')
+  response.setHeader('Access-Control-Allow-Method', '*')
+  response.send('hello') // 只接受字符串类型
 })
 app.listen(8000, () => {
-    console.log('服务已启动');
+  console.log('服务已启动')
 })
--- ES6/js/util.promisify方法.js
+++ ES6/js/util.promisify方法.js
@@ -1,11 +1,11 @@
-//引入util模块
+// 引入util模块
 const util = require('util')
-//引入fs模块
+// 引入fs模块
 const fs = require('fs')
 // 返回一个新的函数
-let mineReadFile = util.promisify(fs.readFile) //借助util.promisify方法将回调函数风格的方法转变为promise的方法
+let mineReadFile = util.promisify(fs.readFile) // 借助util.promisify方法将回调函数风格的方法转变为promise的方法
 mineReadFile('../../NODE/test.txt').then(value => {
-    console.log(value.toString());
+  console.log(value.toString())
 }, reason => {
-    console.log(reason,'1111111');
+  console.log(reason, '1111111')
 })
