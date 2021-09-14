594ce4c45c127bf824e5fc5ef32a7b4d32aa79e0
liuwei <1033615602@qq.com>
2021-09-14 10:43:56 +0800

    修改

--- ES6/12.promise实践.js
+++ ES6/12.promise实践.js
@@ -1,16 +1,16 @@
-const fs = require('fs');
+const fs = require('fs')
 // fs.readFile('../NODE/test.txt',(err,data) => {
 //     if(err) throw err;
 //     console.log(data.toString());
 // })
 let p = new Promise((resolve, reject) => {
-    fs.readFile('../NODE/test.txt', (err, data) => {
-        if (err) reject(err);
-        resolve(data.toString())
-    })
+  fs.readFile('../NODE/test.txt', (err, data) => {
+    if (err) reject(err)
+    resolve(data.toString())
+  })
 })
 p.then(value => {
-    console.log(value);
-},reason=>{
-    console.log(reason);
+  console.log(value)
+}, reason => {
+  console.log(reason)
 })
--- NODE/类型注解.ts
+++
@@ -1,9 +0,0 @@
-(() => {
-    function sayHi(str:string) {
-        // str是string类型的
-        return '你好' + str
-    }
-    let name = "张三"
-    console.log(sayHi(name));
-    
-})()
